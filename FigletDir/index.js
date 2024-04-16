5cdef0723cc3ed39ce3db2b1408b09ada37b41db
Ritesh Kumar <riteshkumar04294@gmail.com>
2024-04-16 20:23:18 +0530

    Figlet_JS
    Figlet is a fascinating tool that allows you to create artistic and eye-catching ASCII art from plain text. It’s like turning your words into captivating visual designs! 🎨.
    1.Figlet is originally a program for transforming regular text into large, stylized letters.
    2.Figlet-JS is a JavaScript implementation of a FIGdriver (the engine behind Figlet). It’s available both as a Node.js module and a jQuery plugin.
    3.You can use Figlet-JS to generate custom ASCII art in your Node.js projects.

---
+++ FigletDir/index.js
@@ -0,0 +1,11 @@
+// var figlet = require("figlet");
+
+// figlet("Rahul Yadav", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
