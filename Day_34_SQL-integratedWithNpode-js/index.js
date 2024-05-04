1d267585adb1aea3f78e4d5a7a64323ee8b3ba76
Ritesh Kumar <riteshkumar04294@gmail.com>
2024-05-04 22:44:28 +0530

    SQL-integratedWithNpode-js

---
+++ Day_34_SQL-integratedWithNpode-js/index.js
@@ -0,0 +1,13 @@
+const { faker }  =  require('@faker-js/faker');
+
+
+let getRandomUser = () => {
+    return {
+      userId: faker.string.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+  }
+
+  console.log(getRandomUser());
